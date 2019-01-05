687fd980b8029be238a5049115808fa301b81f96
Chaman Gurjar <chamangujjar162@gmail.com>
2019-01-05 11:24:08 +0530

    Shown a particular region on map

--- MapsDemoInSwift4/ViewController.swift
+++ MapsDemoInSwift4/ViewController.swift
@@ -10,14 +10,29 @@ import UIKit
 import MapKit
 
 class ViewController: UIViewController, MKMapViewDelegate {
-
+    
     @IBOutlet weak var map: MKMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        showRegionOnMap()
     }
-
-
+    
+    private func showRegionOnMap() {
+        // Loading = > 28.6602997, 77.2396823 - Red fort New Delhi India
+        
+        let latitude: CLLocationDegrees = 28.6602997
+        let longtitude: CLLocationDegrees = 77.2396823
+        //letDelta => Zoom level of the map
+        let letDelta = 0.05
+        let longDelta = 0.05
+        let span = MKCoordinateSpan(latitudeDelta: letDelta, longitudeDelta: longDelta)
+        let coordinates = CLLocationCoordinate2D(latitude: latitude, longitude: longtitude)
+        let region = MKCoordinateRegion(center: coordinates, span: span)
+        map.setRegion(region, animated: true)
+    }
+    
+    
 }
 
